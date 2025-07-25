4e5b44a00f04dfafb44d2e46076008f4fe8d60a3
RifaMolS <rifamolbrkt@gmail.com>
2025-07-25 22:54:16 +0530

    Add files via upload

---
+++ backend/routes/mail.routes.js
@@ -0,0 +1,12 @@
+var express = require('express');
+var router =express.Router();
+
+
+const mailController = require('../controller/mailer.controller');
+
+
+
+router.post("/forgot-password", mailController.sendResetEmail);
+router.post("/reset-password/:token", mailController.resetPassword);
+
+module.exports=router;
